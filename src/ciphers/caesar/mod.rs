d9f5d2b423cc8ca5161740b70134b65a0afbb533
Siqi Yan <syan235711@gmail.com>
2025-09-10 19:46:00 +0100

    Rewrite caesar shift code to one fuction
    Previously, both the encrypt and decrypt functions were essentially the same code repeated, so i rewrote it into one function that both functions call

--- src/ciphers/caesar/mod.rs
+++ src/ciphers/caesar/mod.rs
@@ -6,45 +6,27 @@ fn i8(x: u8) -> i8 {
 
 #[pyfunction]
 pub fn encrypt(input: &str, shift: i32) -> PyResult<String> {
-    // Validate shift range
-    if (-25..=25).contains(&shift) == false {
-        return Err(pyo3::exceptions::PyValueError::new_err(
-            "Shift must be in the range -25 to 25",
-        ));
-    }
-    // Convert shift to u8 and handle wrap-around
-    let shift: i8 = (shift.rem_euclid(26)) as i8;
-    // Main encryption logic
-    let mut result: Vec<u8> = Vec::with_capacity(input.len());
-    for &byte in input.as_bytes() {
-        let byte_num: i8 = i8(byte);
-        let base: i8 = if byte.is_ascii_lowercase() {
-            i8(b'a')
-        } else if byte.is_ascii_uppercase() {
-            i8(b'A')
-        } else {
-            let shifted_byte: u8 = byte as u8;
-            result.push(shifted_byte);
-            continue;
-        };
-        let shifted_byte: u8 = ((((byte_num - base + shift)).rem_euclid(26)) + base) as u8;
-        result.push(shifted_byte);
-    }
-    let result_string: String = String::from_utf8(result)?;
-    Ok(result_string)
+    let result: String = decrypt_or_encrypt(input, shift, "encrypt")?;
+    Ok(result)
 }
 
 #[pyfunction]
 pub fn decrypt(input: &str, shift: i32) -> PyResult<String> {
+    let result: String = decrypt_or_encrypt(input, shift, "decrypt")?;
+    Ok(result)
+}
+
+fn decrypt_or_encrypt(input: &str, shift: i32, mode: &str) -> PyResult<String> {
     // Validate shift range
     if (-25..=25).contains(&shift) == false {
         return Err(pyo3::exceptions::PyValueError::new_err(
             "Shift must be in the range -25 to 25",
         ));
     }
+    // ! Note: I used i8 here because of integer undeflow/overflow issues
     // Convert shift to u8 and handle wrap-around
     let shift: i8 = (shift.rem_euclid(26)) as i8;
-    // Main decryption logic
+    // Main encryption/decryption logic
     let mut result: Vec<u8> = Vec::with_capacity(input.len());
     for &byte in input.as_bytes() {
         let byte_num: i8 = i8(byte);
@@ -57,7 +39,17 @@ pub fn decrypt(input: &str, shift: i32) -> PyResult<String> {
             result.push(shifted_byte);
             continue;
         };
-        let shifted_byte: u8 = ((((byte_num - base - shift)).rem_euclid(26)) + base) as u8;
+        let shifted_byte: u8 = if mode == "encrypt" {
+            println!("Encrypting");
+            ((((byte_num - base + shift)).rem_euclid(26)) + base) as u8
+        } else if mode == "decrypt" {
+            println!("Decrypting");
+            ((((byte_num - base - shift)).rem_euclid(26)) + base) as u8
+        } else {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "Mode must be either 'encrypt' or 'decrypt'",
+            ));
+        };
         result.push(shifted_byte);
     }
     let result_string: String = String::from_utf8(result)?;
