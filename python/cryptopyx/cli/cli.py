0dbebc0967ddb13515a350d0489e282ae8c4cdab
Siqi Yan <syan235711@gmail.com>
2025-11-12 18:21:55 +0000

    Added CLI version option

--- python/cryptopyx/cli/cli.py
+++ python/cryptopyx/cli/cli.py
@@ -3,6 +3,11 @@ import click
 
 # This is the main CLI group, for importing in subcommand modules.
 @click.group()
+@click.version_option(
+    prog_name='CryptoPyX', 
+    package_name='cryptopyx',
+    message='%(prog)s v%(version)s'
+)
 def cli() -> None:
     """CryptoPyX: A Python library for cryptographic operations."""
     pass
