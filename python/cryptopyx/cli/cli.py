75beba41145c363645044b50c892a55ff340afb2
Siqi Yan <syan235711@gmail.com>
2025-11-12 19:08:35 +0000

    Warniing added

--- python/cryptopyx/cli/cli.py
+++ python/cryptopyx/cli/cli.py
@@ -2,6 +2,7 @@ import click
 
 
 # This is the main CLI group, for importing in subcommand modules.
+# Do not import the cli object from this file, use main.py instead.
 @click.group
 @click.version_option(
     prog_name='CryptoPyX', package_name='cryptopyx', message='%(prog)s v%(version)s'
