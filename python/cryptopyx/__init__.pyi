d1b0f7c339efe69e3ce15eb417554632769af8df
Siqi Yan <syan235711@gmail.com>
2025-11-15 10:26:43 +0000

    Fix up some docstrings

--- python/cryptopyx/__init__.pyi
+++ python/cryptopyx/__init__.pyi
@@ -1,5 +1,5 @@
 """
-cryptopyx - a python cryptography package written in Rust made for speed.
+CryptoPyX - a python cryptography package written in Rust made for speed.
 """
 
 from . import ciphers, encodings
--- python/cryptopyx/ciphers/vigenere.pyi
+++ python/cryptopyx/ciphers/vigenere.pyi
@@ -10,17 +10,15 @@ def encrypt(data: str, key: str, skip_non_alpha: bool = True) -> str:
     :type data: str
     :param key: The key to encrypt the data.
     :type key: str
-    :param skip_non_alpha: Whether to skip non-alphabetic characters (optional,
-                          default is True).
+    :param skip_non_alpha: Whether to skip non-alphabetic characters (optional, default is True).
     :type skip_non_alpha: bool
     :return: The encrypted string.
     :rtype: str
 
     :raises ValueError: If the key is invalid,
-                        **i.e**. it's empty or contains non-alphabetic characters,
-                                 though lowercase and uppercase characters are
-                                 both supported.
-                        *or*, if data is non-ascii.
+        **i.e**. it's empty or contains non-alphabetic characters,
+        though lowercase and uppercase characters are both supported.
+        *or*, if data is non-ascii.
 
     """
     ...
@@ -33,16 +31,14 @@ def decrypt(data: str, key: str, skip_non_alpha: bool = True) -> str:
     :type data: str
     :param key: The key to decrypt the data.
     :type key: str
-    :param skip_non_alpha: Whether to skip non-alphabetic characters (optional,
-                          default is True).
+    :param skip_non_alpha: Whether to skip non-alphabetic characters (optional, default is True).
     :type skip_non_alpha: bool
     :return: The decrypted string.
     :rtype: str
 
     :raises ValueError: If the key is invalid,
-                        **i.e**. it's empty or contains non-alphabetic characters,
-                                 though lowercase and uppercase characters are
-                                 both supported.
-                        *or*, if data is non-ascii.
+        **i.e**. it's empty or contains non-alphabetic characters,
+        though lowercase and uppercase characters are both supported.
+        *or*, if data is non-ascii.
     """
     ...
