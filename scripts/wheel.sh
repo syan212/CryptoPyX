9a021a4fdb5239074acb28e379489b5320425c9f
Siqi Yan <syan235711@gmail.com>
2025-11-17 21:35:15 +0000

    Switch from `requirements.txt` To pyproject.toml

--- scripts/wheel.sh
+++ scripts/wheel.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# Requires maturin to be installed first and python>=3.11
 pip uninstall cryptopyx -y
 rm -r dist
+set -e
 maturin build --release --out dist
-pip install dist/* --force-reinstall
+WHEEL_PATH=$(echo ./dist/cryptopyx-*.whl)
+echo $WHEEL_PATH
+pip install $WHEEL_PATH[dev, dev-docs] --force-reinstall
