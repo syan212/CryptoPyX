99d2a5aa40a79518d3a448f6b273261590a1b5bb
Siqi Yan <syan235711@gmail.com>
2025-11-19 19:01:30 +0000

    Update install.sh and CI.yml
    Ensures that the CI, which runs python 3.10, doesn't install docs dependencies, which requires python 3.11

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# Requires python>=3.11 for doc dependencies
-pip install --group dev --group docs
+# Requires python>=3.11 for doc dependencies if --no-docs is not passed in
+if [[ $1 == "--no-docs" ]]; then 
+    pip install --group dev
+else
+    pip install --group dev --group docs
+fi
 maturin build --release --out dist
 pip install dist/cryptopyx-*.whl --force-reinstall
