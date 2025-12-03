faa4c208d6d2ab1fd75e043285bed93878694042
Siqi Yan <syan235711@gmail.com>
2025-12-03 21:54:27 +0000

    Add  option to scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # Requires python>=3.11 for doc dependencies if --no-docs is not passed in
-if [[ $1 == "--no-docs" ]]; then 
+set -e
+if [[ $1 == "--no-docs" || $2 == "--no-docs" ]]; then 
     pip install --group dev
 else
     pip install --group dev --group docs
 fi
-maturin build --release --out dist
+if [[ $1 == "--debug" || $2 == "--debug" ]]; then
+    maturin build --out dist
+else
+    maturin build --release --out dist
+fi
 pip install dist/cryptopyx-*.whl --force-reinstall
