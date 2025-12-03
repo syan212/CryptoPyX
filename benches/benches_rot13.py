6d46c777a2be425edd8ae08a647e69f46c407ea0
Siqi Yan <syan235711@gmail.com>
2025-12-03 21:54:55 +0000

    Update benchmark to remove redundant bench

--- benches/benches_rot13.py
+++ benches/benches_rot13.py
@@ -3,7 +3,7 @@ from utils.strings import random_string
 
 
 # Benchmarks (cause why not?)
-def test_rot13_encrypt_benchmark(benchmark):
+def test_rot13_rotate_benchmark(benchmark):
     strings: list[str] = random_string(
         1000000, string_num=100
     )  # 1 million chars, 100 strings
@@ -13,15 +13,3 @@ def test_rot13_encrypt_benchmark(benchmark):
             rot13.encrypt(string)
 
     benchmark(multi_encrypt)
-
-
-def test_rot13_decrypt_benchmark(benchmark):
-    strings: list[str] = random_string(
-        1000000, string_num=100
-    )  # 1 million chars, 100 strings
-
-    def multi_decrypt():
-        for string in strings:
-            rot13.decrypt(string)
-
-    benchmark(multi_decrypt)
