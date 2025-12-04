5356b7539155a0613d57168614cf0561ed961c64
Siqi Yan <syan235711@gmail.com>
2025-12-04 21:36:33 +0000

    Update benches and shell scripts

--- scripts/benches.sh
+++ scripts/benches.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+set -e
+
 # Run benchmark thrice
-pytest --benchmark-only --benchmark-save=$1
-pytest --benchmark-only --benchmark-save=$1
-pytest --benchmark-only --benchmark-save=$1
+if [[ $2 != "" ]]; then
+    echo "Running benchmarks $2 times..."
+    for _ in {1..$2}; do
+        pytest --benchmark-only --benchmark-save=$1
+    done
+else
+    echo "Running benchmarks thrice..."
+    for _ in {1..3}; do
+        pytest --benchmark-only --benchmark-save=$1
+    done
+fi
+
 # Compare(Uses globbing for max compatibility)
-pytest-benchmark compare '*-*-3.*-*bit/*'
+pytest-benchmark compare '*-*-3.*-*bit/*' --csv .benchmarks/$1
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 # Requires python>=3.11 for doc dependencies if --no-docs is not passed in
 set -e
+
 if [[ $1 == "--no-docs" || $2 == "--no-docs" ]]; then 
     pip install --group dev
 else
     pip install --group dev --group docs
 fi
+
 if [[ $1 == "--debug" || $2 == "--debug" ]]; then
     maturin build --out dist
 else
     maturin build --release --out dist
 fi
+
 pip install dist/cryptopyx-*.whl --force-reinstall
