5f40dab0e8ba27024800de42e4dd9c7bccb03304
Siqi Yan <syan235711@gmail.com>
2025-12-07 15:53:56 +0000

    Formatting!!! And clippy

--- src/encodings/base64.rs
+++ src/encodings/base64.rs
@@ -1,7 +1,8 @@
 use pyo3::prelude::*;
 
 // Standard Base64 alphabet
-const STANDARD_BASE_64_ALPHABET: &[u8; 64] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const STANDARD_BASE_64_ALPHABET: &[u8; 64] =
+    b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 // Exposed python functions
 #[pyfunction]
@@ -12,14 +13,13 @@ pub fn encode_bytes(data: &[u8]) -> PyResult<Vec<u8>> {
 // Bytes encoding
 pub fn encode_bytes_rust(bytes: &[u8]) -> Vec<u8> {
     // Preallocate out vector
-    let mut out: Vec<u8> = Vec::with_capacity((bytes.len() + 2) / 3 * 4);
+    let mut out: Vec<u8> = Vec::with_capacity(bytes.len().div_ceil(3) * 4);
     // Unroll input
     let mut i = 0;
     while i + 3 <= bytes.len() {
         // Read 3 bytes as 24 bits
-        let chunk = ((bytes[i] as u32) << 16) |
-            ((bytes[i + 1] as u32) << 8) |
-            (bytes[i + 2] as u32);
+        let chunk =
+            ((bytes[i] as u32) << 16) | ((bytes[i + 1] as u32) << 8) | (bytes[i + 2] as u32);
         // Extract 4 groups of 6 bits
         out.push(STANDARD_BASE_64_ALPHABET[((chunk >> 18) & 0x3F) as usize]);
         out.push(STANDARD_BASE_64_ALPHABET[((chunk >> 12) & 0x3F) as usize]);
@@ -44,7 +44,7 @@ pub fn encode_bytes_rust(bytes: &[u8]) -> Vec<u8> {
         // Push remainder bytes
         out.push(STANDARD_BASE_64_ALPHABET[((triple >> 18) & 0x3F) as usize]);
         out.push(STANDARD_BASE_64_ALPHABET[((triple >> 12) & 0x3F) as usize]);
-        out.push(STANDARD_BASE_64_ALPHABET[((triple >>  6) & 0x3F) as usize]);
+        out.push(STANDARD_BASE_64_ALPHABET[((triple >> 6) & 0x3F) as usize]);
         // Padding
         out.push(b'=');
     }
