7ec7e89a08bf5c50b7268ca080282f4437d8782c
Siqi Yan <syan235711@gmail.com>
2025-12-09 20:14:16 +0000

    Add scripts/analyse_csv.py scripts

--- scripts/README.md
+++ scripts/README.md
@@ -1,10 +1,12 @@
-# Shell scripts
+# Scripts
 
-These are shell scripts for my convenience
+These are scripts for my convenience
 
 benches.sh - runs benchmarks thrice, and compares them
 
 install.sh -  Installs dev and doc dependencies and installs the actual package
+
+analyse_csv.py - Prints out average of field from csv
 
 CI.sh - runs CI locally. Note that this will not be the same as the github CI, 
         as it only runs it on your specific OS and architecture
---
+++ scripts/analyse_csv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import csv
+from pathlib import Path
+
+
+def _benchmark_matches(row: dict, benchmark_name: str) -> bool:
+    return row['name'] == benchmark_name
+
+
+file_path: Path = (
+    Path(__file__).parent.parent
+    / f'.benchmarks/{input("Enter benchmark file name (without.csv): ")}.csv'
+)
+field: str = input('Enter field name to analyse: ')
+benchmark_name: str = input('Enter benchmark name to filter (leave blank for all): ')
+
+
+with file_path.open(encoding='utf8') as f:
+    reader = csv.DictReader(f)
+    info = [
+        float(row[field])
+        for row in reader
+        if (benchmark_name and _benchmark_matches(row, benchmark_name))
+        or (not benchmark_name)
+    ]
+
+
+average: float = sum(info) / len(info) if info else 0.0
+print(f'Average {field}: {average}')
