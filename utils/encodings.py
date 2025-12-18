35ef2698466b6e23e5ab6d82ca36ae2b439b1bcc
Siqi Yan <syan235711@gmail.com>
2025-12-18 20:40:44 +0000

    style: fix edited files
    Add newline at end of file in both

--- utils/encodings.py
+++ utils/encodings.py
@@ -19,9 +19,7 @@ def random_base64(length: int, string_num: int = 100) -> list[str]:
     """Generate random base64 string. For testing only"""
     random.seed(31415)
     out = []
-    alphabet = (
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    )
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     for _ in range(string_num):
         s = ''.join(random.choice(alphabet) for _ in range(length))
         out.append(s)
