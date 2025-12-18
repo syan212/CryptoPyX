7d2f8b9c7a6cc65d4ca38fe47c31e7632749b545
Siqi Yan <syan235711@gmail.com>
2025-12-18 15:33:44 +0000

    Update uv stuff
    Includes installation of wheel

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,9 @@
 set -e
 
 if [[ $1 == "--no-docs" || $2 == "--no-docs" ]]; then 
-    uv sync --no-install-project --frozen --inexact
+    uv sync --frozen --inexact
 else
-    uv sync --no-install-project --group dev --group docs --frozen --inexact
+    uv sync --group dev --group docs --frozen --inexact
 fi
 
-if [[ $1 == "--debug" || $2 == "--debug" ]]; then
-    uv run maturin build --out dist
-else
-    uv run maturin build --release --out dist
-fi
-
-uv pip install dist/cryptopyx-*.whl --force-reinstall
+uv pip install -e .
