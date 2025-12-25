530b8f34c6959ae780ce74561e63a6f2a2e08add
Siqi Yan <syan235711@gmail.com>
2025-12-25 20:31:38 +0000

    feat(cli): add base64 cli matching (still wip)

--- src/cli/matches.rs
+++ src/cli/matches.rs
@@ -70,7 +70,7 @@ pub fn get_matches() -> Command {
                 )
                 .subcommand(
                     Command::new("decode")
-                        .about("Decode a string into base32.")
+                        .about("Decode base32 into a string.")
                         .arg(
                             Arg::new("data")
                                 .help("Data to decode. Can be a file name (default behaviour) or a raw string (pass in -s).")
@@ -91,6 +91,57 @@ pub fn get_matches() -> Command {
                                 .action(ArgAction::Set),
                         ),
                 ),
+        )
+        .subcommand(
+            Command::new("base64")
+                .subcommand_required(true)
+                .about("Base64 encoding and decoding.")
+                .subcommand(
+                    Command::new("encode")
+                        .about("Encode a string into base64")
+                        .arg(
+                            Arg::new("data")
+                                .help("Data to encode. Can be a file name (default behaviour) or a raw string (pass in -s).")
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::new("string")
+                            .help("Flag to indicate that the input data is a string.")
+                            .short('s')
+                            .long("string")
+                            .action(ArgAction::SetTrue),
+                        )
+                        .arg(
+                            Arg::new("output")
+                                .help("Sets the file output of the encoded data. Default is stdout.")
+                                .short('o')
+                                .long("output")
+                                .action(ArgAction::Set),
+                        ),
+                )
+                .subcommand(
+                    Command::new("decode")
+                        .about("Decode base64 into a string")
+                        .arg(
+                            Arg::new("data")
+                                .help("Data to decode. Can be a file name (default behaviour) or a raw string (pass in -s).")
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::new("string")
+                                .help("Flag to indicate that the input data is a string.")
+                                .short('s')
+                                .long("string")
+                                .action(ArgAction::SetTrue),
+                        )
+                        .arg(
+                            Arg::new("output")
+                                .help("Sets the file output of the decoded data. Default is stdout.")
+                                .short('o')
+                                .long("output")
+                                .action(ArgAction::Set),
+                        ),
+                )
         )
         .styles(get_styles())
 }
