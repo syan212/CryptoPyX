ad943d5ea774fddf530caed6d18ccde280defe22
Siqi Yan <syan235711@gmail.com>
2025-12-25 22:05:22 +0000

    chore: update ci scripts

--- scripts/CI.sh
+++ scripts/CI.sh
@@ -4,13 +4,17 @@ shopt -s globstar
 
 # Pytest(No no benchmark)
 uv run pytest --benchmark-skip
+echo "Passed pytest testing"
 
 # Ruff formatting and linting
 uv run ruff check .
 uv run ruff format --diff
+echo "Passed ruff formatting and linting"
 
 # Rustfmt
 rustfmt --check src/**/*.rs
+echo "Passed rustfmt formatting"
 
 # Clippy
 uv run cargo clippy --all-targets --all-features -- -D warnings
+echo "Passed clippy linting"
